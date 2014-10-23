122fd1f9c1bb26ad6c48d0728d3204c0e0ef457d
Xin Xu <railwaycat@gmail.com>
2014-10-23 00:44:09 -0400

    fix crash on 10.9.x

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,7 +1,7 @@
 class EmacsMac < Cask
   version 'emacs-24.4-mac-5.0'
 
-  sha256 '3a766e86ef92f3b3850722179efc5a9e14b61683cab867cfe4a0a2f11a9445c8'
+  sha256 '7e09c899e5487c21634aea4220101f739227fcbbcd34f7998d2a98e87fc00aa2'
 
   url 'https://s3.amazonaws.com/emacs-mac-port/emacs-mac-port-24.4-mac-5.0.zip'
   homepage 'http://www.gnu.org/software/emacs/'
