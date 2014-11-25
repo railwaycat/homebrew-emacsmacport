9ef5d4488e2a9bc74d9b22201bbd37aec719a9fd
Xin Xu <railwaycat@gmail.com>
2014-11-25 16:02:54 -0500

    fix cask install, not sure if old-version users need an uninstall first

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,4 +1,4 @@
-class EmacsMac < Cask
+cask :v1 => 'emacs-mac' do
   version 'emacs-24.4-mac-5.2'
 
   sha256 '7d37b9a4d444853c1caa7caa7a931f91d0763e217457629800535daf5f141df5'
@@ -6,5 +6,5 @@ class EmacsMac < Cask
   url 'https://s3.amazonaws.com/emacs-mac-port/emacs-mac-port-24.4-mac-5.2.zip'
   homepage 'http://www.gnu.org/software/emacs/'
 
-  link 'Emacs.app'
+  app 'Emacs.app'
 end
