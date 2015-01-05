b53fb28c4d79e3a61ff02ba6e2e7750f7ede38a2
Casper Hollingberry <hollingberry@users.noreply.github.com>
2015-01-04 23:12:00 -0800

    Clean up emacs-mac formula

--- Formula/emacs-mac.rb
+++ Formula/emacs-mac.rb
@@ -23,35 +23,27 @@ class EmacsMac < Formula
   depends_on 'libxml2' if build.include? 'with-xml2'
 
   def caveats
-    s = ""
-    s += <<-EOS.undent
-
+    <<-EOS.undent
       This is YAMAMOTO Mitsuharu's experimental "Mac port" addition to
       GNU Emacs 24. This provides a native GUI support for Mac OS X
       10.4 - 10.10. After installing, see README-mac and NEWS-mac
       in #{prefix} for the port details.
 
+      Emacs.app was installed to:
+        #{prefix}
+
+      To link the application to default Homebrew App location:
+        brew linkapps
+      or:
+        ln -s #{prefix}/Emacs.app /Applications
+
+      For a CLI starter, please look at:
+        https://gist.github.com/4043945
+
+      To install info files under share/info, an environment variable
+      need been set before install. Read more: man brew.
+        export HOMEBREW_KEEP_INFO=1
     EOS
-
-    s += <<-EOS.undent
-        Emacs.app was installed to:
-          #{prefix}
-
-        To link the application to default Homebrew App location:
-          brew linkapps
-        or:
-          ln -s #{prefix}/Emacs.app /Applications
-
-        For a CLI starter, please look at:
-          https://gist.github.com/4043945
-
-        To install info files under share/info, an environment variable
-        need been set before install. Read more: man brew.
-          export HOMEBREW_KEEP_INFO=1
-
-    EOS
-
-    return s
   end
 
   # Follow Homebrew and don't install ctags from Emacs. This allows Vim
@@ -64,20 +56,22 @@ class EmacsMac < Formula
   end
 
   def install
-    args = ["--prefix=#{prefix}",
-            "--enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp",
-            "--infodir=#{info}/emacs",
-            "--with-mac",
-            "--enable-mac-app=#{prefix}"]
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp",
+      "--infodir=#{info}/emacs",
+      "--with-mac",
+      "--enable-mac-app=#{prefix}"
+    ]
 
     # icons
-    icon_path = "./mac/Emacs.app/Contents/Resources/"
+    icons = "./mac/Emacs.app/Contents/Resources"
     if build.include? "icon-official"
-      system "rm -f "+icon_path+"Emacs.icns"
-      system "cp " +icon_path+"Emacs.icns.official "+icon_path+"Emacs.icns"
+      system "rm -f #{icons}/Emacs.icns"
+      system "cp #{icons}/Emacs.icns.official #{icons}/Emacs.icns"
     elsif build.include? "icon-modern"
-      system "rm -f "+icon_path+"Emacs.icns"
-      system "cp " +icon_path+"Emacs.icns.modern "+icon_path+"Emacs.icns"
+      system "rm -f #{icons}/Emacs.icns"
+      system "cp #{icons}/Emacs.icns.modern #{icons}/Emacs.icns"
     end
     
     # build
