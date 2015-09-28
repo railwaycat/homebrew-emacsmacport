ed2b8cf2b1ede23dbd1e08928082d2eaa07d9984
Xin Xu <railwaycat@gmail.com>
2015-09-28 14:35:48 -0400

    emacs-24.5-mac-5.10 for casks

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'emacs-mac' do
-  version 'emacs-24.5-z-mac-5.10'
+  version 'emacs-24.5-z-mac-5.11'
 
-  sha256 '2983898fdba78d4da6734497a43ccec57bcb4bd2ed73fe4f911fc9d6452a2e62'
+  sha256 'c243705dc5e3fea0cfe70b9530ee9382c402445b3b3b7712902293af01d04078'
 
-  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-24.5-mac-5.10.zip'
+  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-24.5-mac-5.11.zip'
   homepage 'https://www.gnu.org/software/emacs/'
 
   app 'Emacs.app'
