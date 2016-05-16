e194c279e5d71fad0bfa107d27ae36447167caec
Xin Xu <railwaycat@gmail.com>
2016-05-16 19:14:40 -0400

    fix brew cask audit failures

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -4,7 +4,9 @@ cask 'emacs-mac' do
   sha256 '7e5a32ecf298fe8b1f51ea2b17fdd6e02242ffeea5feee7914b664056bc06ca7'
 
   url 'https://s3.amazonaws.com/emacs-mac-port/emacs-24.5-mac-5.17.zip'
+  name 'Emacs-mac'
   homepage 'https://www.gnu.org/software/emacs/'
+  license :gpl
 
   app 'Emacs.app'
 end
