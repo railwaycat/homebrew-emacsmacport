9fc79043dfe64fd44f74ca0e876b8344db70c660
Xin Xu <railwaycat@gmail.com>
2016-07-27 01:23:49 -0400

    emacs-25.1-rc1-mac-5.90 for cask

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,9 +1,9 @@
 cask 'emacs-mac' do
-  version 'emacs-24.5-z-mac-5.18'
+  version 'emacs-25.1-rc1-mac-5.90'
 
-  sha256 '64092b79102dd085884090a8f2a5e88c5a01ba90b48400a08eadcc09965544a1'
+  sha256 '173d10eb7e720b0c72d5fbeb3aded2d380f6de43dc8e5c890a2836e508a575bf'
 
-  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-24.5-mac-5.18.zip'
+  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-25.1-rc1-mac-5.90.zip'
   name 'Emacs-mac'
   homepage 'https://www.gnu.org/software/emacs/'
   license :gpl
