bb3da78f7b114e54dabbf3ea8af2a6575fa66fd0
Xin Xu <railwaycat@gmail.com>
2016-11-05 15:48:52 -0400

    fix cask shasum for signed bundle

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,7 +1,7 @@
 cask 'emacs-mac' do
   version 'emacs-25.1-z-mac-6.1'
 
-  sha256 'bd132f46f90014fd23cbe2f38e32257a4e1b4b5f77144bcb10f9f0bd011eb0f3'
+  sha256 '41230bfe0f87b3e58fe6c2f747cbfc688b4d9623728f9509fe654c4116c81877'
 
   url 'https://s3.amazonaws.com/emacs-mac-port/emacs-25.1-mac-6.1.zip'
   name 'Emacs-mac'
