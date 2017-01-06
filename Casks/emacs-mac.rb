7c047ec27d997fa812280c8ed7ff06edfb609a1f
Wes Morgan <wesmorgan@icloud.com>
2017-01-06 12:42:11 -0700

    Use official icon in cask
    Since the cask is pre-built and not really configurable, perhaps it should default to the official icon?

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -3,7 +3,7 @@ cask 'emacs-mac' do
 
   sha256 '41230bfe0f87b3e58fe6c2f747cbfc688b4d9623728f9509fe654c4116c81877'
 
-  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-25.1-mac-6.1.zip'
+  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-25.1-mac-6.1-official-icon.zip'
   name 'Emacs-mac'
   homepage 'https://www.gnu.org/software/emacs/'
   license :gpl
