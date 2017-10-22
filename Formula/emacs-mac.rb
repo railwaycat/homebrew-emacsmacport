6a80cc29d7b256e803a9778ba5d56108a72cfdbf
Gregory Werbin <gwerbin@users.noreply.github.com>
2017-10-22 14:54:38 -0400

    Add the `plist` block from homebrew-core
    This allows you to use `brew services` to run Emacs.
    
    This is literally just copied and pasted from the homebrew-core Emacs formula, but it works on my machine.™

--- Formula/emacs-mac.rb
+++ Formula/emacs-mac.rb
@@ -125,6 +125,25 @@ class EmacsMac < Formula
     end
   end
 
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/emacs</string>
+        <string>--daemon</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
   def caveats
     <<-EOS.undent
       This is YAMAMOTO Mitsuharu's "Mac port" addition to
