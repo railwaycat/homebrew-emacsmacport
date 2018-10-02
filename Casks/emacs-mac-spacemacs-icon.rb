dc1d2a5ad5c780f837224c621a40fc7f1ef92b7d
Xin Xu <railwaycat@gmail.com>
2018-10-02 11:17:07 -0400

    update cask for emacs-26.1-mac-7.2 and new built for '10.14 and other'

--- Casks/emacs-mac-spacemacs-icon.rb
+++ Casks/emacs-mac-spacemacs-icon.rb
@@ -1,11 +1,32 @@
 cask 'emacs-mac-spacemacs-icon' do
-  version 'emacs-26.1-z-mac-7.1'
+  version 'emacs-26.1-z-mac-7.2'
 
-  sha256 'efd9e32fa8d71a6f92758bb83a82c99df879188b7960c717eb1c5d6d90b9fbd2'
+  if MacOS.version >= :mojave
+    sha256 '1dbfbe8b34c5b866f65deaba18f996577725af4ad9c685f819cb795f6f088021'
+    url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.2-10.14-spacemacs-icon.zip'
+  else
+    sha256 '2cad2949bf6dec4a44f806089e0827474b00e50ea3dd1ef97e516ab66e45b025'
+    url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.2-10.9-spacemacs-icon.zip'
+	end
 
-  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.1-spacemacs-icon.zip'
   name 'Emacs-mac with spacemacs icon'
   homepage 'https://bitbucket.org/mituharu/emacs-mac.git'
 
+  conflicts_with cask: [
+                        'emacs',
+                        'emacs-mac'
+                       ]
+
   app 'Emacs.app'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+
+  zap trash: [
+               '~/Library/Caches/org.gnu.Emacs',
+               '~/Library/Preferences/org.gnu.Emacs.plist',
+               '~/Library/Saved Application State/org.gnu.Emacs.savedState',
+             ]
+
 end
--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,11 +1,32 @@
 cask 'emacs-mac' do
-  version 'emacs-26.1-z-mac-7.1'
+  version 'emacs-26.1-z-mac-7.2'
 
-  sha256 '579c0534f4c3c37303c6b50b757b6c88264555a6f5dec0a5d7562dc417d193ff'
+  if MacOS.version >= :mojave
+    sha256 '8b23f75ced828a06c85966f3ad05e6bd494a466d38e39d9017a93d9ed501f2d9'
+    url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.2-10.14.zip'
+  else
+    sha256 '7edc1aa09f9d20942f2091ba31e5d2b190ae73f7b46834f6844eb974c2e3f6f8'
+    url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.2-10.9.zip'
+	end
 
-  url 'https://s3.amazonaws.com/emacs-mac-port/emacs-26.1-mac-7.1.zip'
   name 'Emacs-mac'
   homepage 'https://bitbucket.org/mituharu/emacs-mac.git'
 
+  conflicts_with cask: [
+                        'emacs',
+                        'emacs-mac-spacemacs-icon'
+                       ]
+
   app 'Emacs.app'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
+  binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
+
+  zap trash: [
+               '~/Library/Caches/org.gnu.Emacs',
+               '~/Library/Preferences/org.gnu.Emacs.plist',
+               '~/Library/Saved Application State/org.gnu.Emacs.savedState',
+             ]
+
 end
