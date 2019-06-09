3046a795334da397f55d81a6fb584248bce84710
Xin Xu <railwaycat@gmail.com>
2019-06-08 20:43:34 -0400

    Revert "update cask for future unofficial release emacs-26.2-mac-7.6.1"
    This reverts commit 2540f475b4d707351d4e055908851dbb55de7669.

--- Casks/emacs-mac-spacemacs-icon-snapshot.rb
+++ Casks/emacs-mac-spacemacs-icon-snapshot.rb
@@ -1,26 +1,26 @@
-cask 'emacs-mac-spacemacs-icon' do
-  version 'emacs-26.2-z-mac-7.6'
+cask 'emacs-mac-spacemacs-icon-snapshot' do
+  version 'emacs-mac-a605292'
 
   if MacOS.version >= :mojave
-    sha256 'a88bd208c0079db887da34b7f4b6b517076d8239d11a58936be91d919d4af399'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.14.3-spacemacs-icon.zip'
+    sha256 '92ac5c72a2561d0f4e1a81acaec49cade108c6cc472897da2cc883f9ea5706cb'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-rc1-mac-7.5/emacs-26.2-rc1-mac-7.5-10.14.3-spacemacs-icon.zip'
   elsif MacOS.version == :high_sierra
-    sha256 'b9e4a9b692c84dfb3082c7be45afcdbb157b471182d58e40d2a7f16977bcf84e'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.13.6-spacemacs-icon.zip'
+    sha256 'a5b76a439cd7ce6bd3ba9552b88a1e5279ccd7fac720fd02fa3d62a459d83d21'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-rc1-mac-7.5/emacs-26.2-rc1-mac-7.5-10.13.6-spacemacs-icon.zip'
   else
-    sha256 'c13c6930f3cfe910c1f744ac452af47603e2e187dbf2452a484ea327d15599ed'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.12.6-spacemacs-icon.zip'
+    sha256 '62f865ccf18328ac68bcca74b62d53af035a480e0a772585f7eeffe1605f1dcf'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-rc1-mac-7.5/emacs-26.2-rc1-mac-7.5-10.12.6-spacemacs-icon.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
-  name 'Emacs-mac with spacemacs icon'
+  name 'Emacs-mac-snapshot with spacemacs icon'
   homepage 'https://bitbucket.org/mituharu/emacs-mac.git'
 
   conflicts_with cask: [
                         'emacs',
                         'emacs-mac',
                         'emacs-mac-snapshot',
-                        'emacs-mac-spacemacs-icon-snapshot'
+                        'emacs-mac-spacemacs-icon'
                        ]
 
   app 'Emacs.app'
