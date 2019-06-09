2540f475b4d707351d4e055908851dbb55de7669
Xin Xu <railwaycat@gmail.com>
2019-06-08 20:37:43 -0400

    update cask for future unofficial release emacs-26.2-mac-7.6.1

--- Casks/emacs-mac-spacemacs-icon.rb
+++ Casks/emacs-mac-spacemacs-icon.rb
@@ -1,15 +1,15 @@
 cask 'emacs-mac-spacemacs-icon' do
-  version 'emacs-26.2-z-mac-7.6'
+  version 'emacs-26.2-z-mac-7.6.1'
 
   if MacOS.version >= :mojave
-    sha256 'a88bd208c0079db887da34b7f4b6b517076d8239d11a58936be91d919d4af399'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.14.3-spacemacs-icon.zip'
+    sha256 '4548db2dfc58af3f3a919a62cd99266dd23342e3359aaa5801b3c5f397c93fd4'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.14.3-spacemacs-icon.zip'
   elsif MacOS.version == :high_sierra
-    sha256 'b9e4a9b692c84dfb3082c7be45afcdbb157b471182d58e40d2a7f16977bcf84e'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.13.6-spacemacs-icon.zip'
+    sha256 '9b333b5787cd6ad7ef679bbe504a3c00d1ef814daed0d799122fa1ab5f001496'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.13.6-spacemacs-icon.zip'
   else
-    sha256 'c13c6930f3cfe910c1f744ac452af47603e2e187dbf2452a484ea327d15599ed'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.12.6-spacemacs-icon.zip'
+    sha256 'ed6f6540828dd931b622acf007085229bf94fd092aaee690ae793723db4a7be7'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.12.6-spacemacs-icon.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
@@ -19,12 +19,10 @@ cask 'emacs-mac-spacemacs-icon' do
   conflicts_with cask: [
                         'emacs',
                         'emacs-mac',
-                        'emacs-mac-snapshot',
-                        'emacs-mac-spacemacs-icon-snapshot'
                        ]
 
   app 'Emacs.app'
-  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs.sh", target: 'emacs'
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,15 +1,15 @@
 cask 'emacs-mac' do
-  version 'emacs-26.2-z-mac-7.6'
+  version 'emacs-26.2-z-mac-7.6.1'
 
   if MacOS.version >= :mojave
-    sha256 '81e3ee9d86ad22c2784a0a5086c87a09226cd037a8bd58e2681607ab2b554018'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.14.3.zip'
+    sha256 '26455fcda7a045ef9285df3634c9a69239a709c78608ac2087513a5b0e1578a6'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.14.3.zip'
   elsif MacOS.version == :high_sierra
-    sha256 'b551f358f50ff720020f5f74211392976165ed9e7d01648ce6885d01fc052cc3'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.13.6.zip'
+    sha256 '72416a2d3e505e023d296a5eabff058644ccde5100f3a5e4c12d588aa6a9874e'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.13.6.zip'
   else
-    sha256 '58bafd25b255440adc424aa4ef92702fb049a20ebf259178b89d56f7b5df91da'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6/emacs-26.2-mac-7.6-10.12.6.zip'
+    sha256 '6ee10e2dd43244ad65f1512fae159e25f6e0cb66d0a93cd7638c29a06446a810'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.2-mac-7.6.1/emacs-26.2-mac-7.6.1-10.12.6.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
@@ -18,13 +18,11 @@ cask 'emacs-mac' do
 
   conflicts_with cask: [
                         'emacs',
-                        'emacs-mac-snapshot',
                         'emacs-mac-spacemacs-icon',
-                        'emacs-mac-spacemacs-icon-snapshot'
                        ]
 
   app 'Emacs.app'
-  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
+  binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs.sh", target: 'emacs'
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/ebrowse"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/emacsclient"
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
