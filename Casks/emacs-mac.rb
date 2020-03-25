bae5053bbcf14fae336917c66c5947515c8ec155
Xin Xu <railwaycat@gmail.com>
2020-03-24 23:01:32 -0400

    emacs-26.3-mac-7.9 for brew cask

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,15 +1,12 @@
 cask 'emacs-mac' do
-  version 'emacs-26.3-z-mac-7.8'
+  version 'emacs-26.3-z-mac-7.9'
 
   if MacOS.version >= :mojave
-    sha256 '161d27e44e2e0863feb5a935d5acc7bf08d676c56370208070b1b5bcdbebd701'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.3-mac-7.8/emacs-26.3-mac-7.8-10.14.4.zip'
-  elsif MacOS.version == :high_sierra
-    sha256 'b36016ae17824f3a3c53a2e35402b9768d5e269a57d22bf774dfb2d5eee5c085'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.3-mac-7.8/emacs-26.3-mac-7.8-10.13.6.zip'
+    sha256 '47a4d542bcbdc9d97bbd49d0482e663370e5c584fd43ac6ca7a0fa29625c68da'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.3-mac-7.9/emacs-26.3-mac-7.9-10.14.4.zip'
   else
-    sha256 '46dfe61d6c0352f02c5e29446fba9a5d0380f0113f0689366e4cee6bd1aab101'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.3-mac-7.8/emacs-26.3-mac-7.8-10.12.6.zip'
+    sha256 '6027df79425f334ff2a2aa943bcafc01f8f3faa5bae2b63fecfa641c963d3045'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-26.3-mac-7.9/emacs-26.3-mac-7.9-10.13.6.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
