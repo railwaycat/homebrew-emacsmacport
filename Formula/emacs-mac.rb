d5d1b980936e6d5c26222d95c2d1f68fed2cec1a
Xin Xu <railwaycat@gmail.com>
2020-08-16 19:23:04 -0400

    satisfy brew audit

--- Formula/emacs-mac.rb
+++ Formula/emacs-mac.rb
@@ -5,13 +5,14 @@ class EmacsMac < Formula
   version "emacs-27.1-mac-8.0"
   sha256 "b6a20eafb475a08d25213d475320f85d6acd438319cb7fe2a51e571d651975be"
 
-  head "https://bitbucket.org/mituharu/emacs-mac.git", :branch => "work"
+  head "https://bitbucket.org/mituharu/emacs-mac.git", branch: "work"
 
   option "with-dbus", "Build with d-bus support"
   option "without-modules", "Build without dynamic modules support"
   option "with-rsvg", "Build with rsvg support"
   option "with-ctags", "Don't remove the ctags executable that emacs provides"
-  option "with-no-title-bars", "Build with a patch for no title bars on frames (not recommended to use with --HEAD option)"
+  option "with-no-title-bars",
+         "Build with a patch for no title bars on frames (not recommended to use with --HEAD option)"
   option "with-natural-title-bar",
          "Build with a patch for title bar color inferred by theme (not recommended to use with --HEAD option)"
   option "with-modern-icon", "Using a modern style Emacs icon by @tpanum"
@@ -23,19 +24,19 @@ class EmacsMac < Formula
   # Update list from
   # https://raw.githubusercontent.com/emacsfodder/emacs-icons-project/master/icons.json
   emacs_icons_project_icons = {
-    "EmacsIcon1" => "50dbaf2f6d67d7050d63d987fe3743156b44556ab42e6d9eee92248c56011bd0",
-    "EmacsIcon2" => "8d63589b0302a67f13ab94b91683a8ad7c2b9e880eabe008056a246a22592963",
-    "EmacsIcon3" => "80dd2a4776739a081e0a42008e8444c729d41ba876b19fa9d33fde98ee3e0ebf",
-    "EmacsIcon4" => "8ce646ca895abe7f45029f8ff8f5eac7ab76713203e246b70dea1b8a21a6c135",
-    "EmacsIcon5" => "ca415df7ad60b0dc495626b0593d3e975b5f24397ad0f3d802455c3f8a3bd778",
-    "EmacsIcon6" => "12a1999eb006abac11535b7fe4299ebb3c8e468360faf074eb8f0e5dec1ac6b0",
-    "EmacsIcon7" => "f5067132ea12b253fb4a3ea924c75352af28793dcf40b3063bea01af9b2bd78c",
-    "EmacsIcon8" => "d330b15cec1bcdfb8a1e8f8913d8680f5328d59486596fc0a9439b54eba340a0",
-    "EmacsIcon9" => "f58f46e5ef109fff8adb963a97aea4d1b99ca09265597f07ee95bf9d1ed4472e",
-    "emacs-card-blue-deep" => "6bdb17418d2c620cf4132835cfa18dcc459a7df6ce51c922cece3c7782b3b0f9",
+    "EmacsIcon1"                      => "50dbaf2f6d67d7050d63d987fe3743156b44556ab42e6d9eee92248c56011bd0",
+    "EmacsIcon2"                      => "8d63589b0302a67f13ab94b91683a8ad7c2b9e880eabe008056a246a22592963",
+    "EmacsIcon3"                      => "80dd2a4776739a081e0a42008e8444c729d41ba876b19fa9d33fde98ee3e0ebf",
+    "EmacsIcon4"                      => "8ce646ca895abe7f45029f8ff8f5eac7ab76713203e246b70dea1b8a21a6c135",
+    "EmacsIcon5"                      => "ca415df7ad60b0dc495626b0593d3e975b5f24397ad0f3d802455c3f8a3bd778",
+    "EmacsIcon6"                      => "12a1999eb006abac11535b7fe4299ebb3c8e468360faf074eb8f0e5dec1ac6b0",
+    "EmacsIcon7"                      => "f5067132ea12b253fb4a3ea924c75352af28793dcf40b3063bea01af9b2bd78c",
+    "EmacsIcon8"                      => "d330b15cec1bcdfb8a1e8f8913d8680f5328d59486596fc0a9439b54eba340a0",
+    "EmacsIcon9"                      => "f58f46e5ef109fff8adb963a97aea4d1b99ca09265597f07ee95bf9d1ed4472e",
+    "emacs-card-blue-deep"            => "6bdb17418d2c620cf4132835cfa18dcc459a7df6ce51c922cece3c7782b3b0f9",
     "emacs-card-british-racing-green" => "ddf0dff6a958e3b6b74e6371f1a68c2223b21e75200be6b4ac6f0bd94b83e1a5",
-    "emacs-card-carmine" => "4d34f2f1ce397d899c2c302f2ada917badde049c36123579dd6bb99b73ebd7f9",
-    "emacs-card-green" => "f94ade7686418073f04b73937f34a1108786400527ed109af822d61b303048f7",
+    "emacs-card-carmine"              => "4d34f2f1ce397d899c2c302f2ada917badde049c36123579dd6bb99b73ebd7f9",
+    "emacs-card-green"                => "f94ade7686418073f04b73937f34a1108786400527ed109af822d61b303048f7",
   }
 
   emacs_icons_project_icons.each_key do |icon|
@@ -48,14 +49,13 @@ class EmacsMac < Formula
 
   depends_on "autoconf"
   depends_on "automake"
+  depends_on "d-bus" if build.with? "dbus"
+  depends_on "gnutls"
+  depends_on "librsvg" if build.with? "rsvg"
   depends_on "pkg-config"
   depends_on "texinfo"
-  depends_on "gnutls"
-
-  depends_on "d-bus" if build.with? "dbus"
-  depends_on "librsvg" if build.with? "rsvg"
-  depends_on "libxml2" => :recommended
   depends_on "jansson" => :recommended
+  depends_on "libxml2" => :recommended
   depends_on "glib" => :optional
   depends_on "imagemagick" => :optional
 
@@ -123,7 +123,7 @@ class EmacsMac < Formula
   patch do
     url "https://gist.github.com/railwaycat/c48cc2ea545a9d29521e092662661311/raw/0c7977aa5ae27e386e0b9d9e3f5d28881abd316d/multi-tty-27.diff"
     sha256 "5a13e83e79ce9c4a970ff0273e9a3a07403cc07f7333a0022b91c191200155a1"
-  end    
+  end
 
   def install
     args = [
