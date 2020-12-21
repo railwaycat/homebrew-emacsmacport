92cd6c9477f2a01543e340ec7f6e0d85b4c87c01
Alexandre Bouayad <55853079+alexandreBouayad@users.noreply.github.com>
2020-12-21 03:10:12 +0100

    Update emacs-mac.rb

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,22 +1,17 @@
-cask 'emacs-mac' do
-  version 'emacs-27.1-mac-8.1'
+cask 'emacs-mac-custom' do
+  version 'emacs-27.1-mac-8.1-custom'
 
-  if MacOS.version >= :mojave
-    sha256 'cd6fec3a33ae9a12fca1d1fad35bfa8f12489d518c1e2bbd207df6d6eb251d01'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.1-mac-8.1/emacs-27.1-mac-8.1-10.14.6.zip'
-  else
-    sha256 '41a02e4193bc722b67426791cf210fd7581f8e4faf28e3feb9b78ced183c2c8e'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.1-mac-8.1/emacs-27.1-mac-8.1-10.13.6.zip'
-  end
+  sha256 'cd6fec3a33ae9a12fca1d1fad35bfa8f12489d518c1e2bbd207df6d6eb251d01'
+  url 'https://github.com/alexandreBouayad/homebrew-emacsmacport/releases/download/emacs-27.1-mac-8.1-custom/emacs-27.1-mac-8.1-custom.zip'
 
-  appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
-  name 'Emacs-mac'
+  name 'Emacs-mac-custom'
   homepage 'https://bitbucket.org/mituharu/emacs-mac.git'
 
   conflicts_with cask: [
-                        'emacs',
-                        'emacs-mac-spacemacs-icon',
-                       ]
+    'emacs',
+    'emacs-mac-spacemacs-icon',
+    'emacs-mac'
+  ]
 
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs.sh", target: 'emacs'
@@ -26,9 +21,9 @@ cask 'emacs-mac' do
   binary "#{appdir}/Emacs.app/Contents/Resources/include/emacs-module.h", target: "#{HOMEBREW_PREFIX}/include/emacs-module.h"
 
   zap trash: [
-               '~/Library/Caches/org.gnu.Emacs',
-               '~/Library/Preferences/org.gnu.Emacs.plist',
-               '~/Library/Saved Application State/org.gnu.Emacs.savedState',
-             ]
+    '~/Library/Caches/org.gnu.Emacs',
+    '~/Library/Preferences/org.gnu.Emacs.plist',
+    '~/Library/Saved Application State/org.gnu.Emacs.savedState',
+  ]
 
 end
