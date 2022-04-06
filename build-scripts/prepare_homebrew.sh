d1c810c58f7c4ece848f52e1bceafc81d424159b
Xin Xu <railwaycat@gmail.com>
2022-04-06 19:22:33 -0400

    bump version and cleanup

--- build-scripts/prepare_homebrew.sh
+++ build-scripts/prepare_homebrew.sh
@@ -6,22 +6,11 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 OS_VER=$(sw_vers -productVersion)
 
-if [ $OS_VER != "10.13.6" ]; then
-  # for version newer than 10.13, we can afford the upgrade time for homebrew
-  # packages. Build with the latest version/.
-  brew update
-  # remove all installed formula and reinstall from Brewfile, prevent waste time
-  # on upgrade all
-  # brew remove --force $(brew list --formula)
-  brew bundle --file $(pwd)/build-scripts/Brewfile
-else
-  # for 10.13, homebrew upgrade package takes a ridiculous time way more than
-  # the time limitation of CI, we have to skip it and use the shipping version
-  # of CI VM.
-  # brew bundle will fail on 10.13
-  brew install texinfo
-  brew install jansson
-fi
+brew update
+# remove all installed formula and reinstall from Brewfile, prevent waste time
+# on upgrade all
+# brew remove --force $(brew list --formula)
+brew bundle --file $(pwd)/build-scripts/Brewfile
 
 # output the version of gnutls
 brew info gnutls
