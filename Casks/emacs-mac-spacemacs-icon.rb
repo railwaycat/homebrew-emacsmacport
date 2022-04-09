c58f34e9ed6b02bb6c3d6c2a07fa5caa8740c1e0
Xin Xu <railwaycat@gmail.com>
2022-04-09 15:46:09 -0400

    add 28.1-mac-9.0 cask

--- Casks/emacs-mac-spacemacs-icon.rb
+++ Casks/emacs-mac-spacemacs-icon.rb
@@ -1,25 +1,27 @@
 cask 'emacs-mac-spacemacs-icon' do
-  version 'emacs-27.2-mac-8.3.1'
+  version 'emacs-28.1-mac-9.0'
 
   if Hardware::CPU.intel?
-    if MacOS.version <= :high_sierra
-      odie "Because of the limitation of build system, there is no cask support for High Sierra(10.13) and earlier, please build from Homebrew formula."
-    elsif MacOS.version <= :mojave
-      sha256 '30fb8ad8114fc61942ea7c1222029c9a81484ca8e9d134ea8623e772e03ed350'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.2-mac-8.3/emacs-27.2-mac-8.3-10.14.6-spacemacs-icon.zip'
+    if MacOS.version <= :mojave
+      odie "Because of the limitation of build system, there is no cask support for mojave(10.14) and earlier, please build from Homebrew formula."
     elsif MacOS.version <= :catalina
-      sha256 '8eb1c1eb7a740322edacd4197ed1cff9264c1527a09ea30250409128d32a33e2'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.2-mac-8.3/emacs-27.2-mac-8.3-10.15.7-spacemacs-icon.zip'
+      sha256 '832e1b9384730cc5ff2ce018a979aec82699a7a13c9bd18f84649a39644d50ac'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.1-mac-9.0/emacs-28.1-mac-9.0-10.15.7-spacemacs-icon.zip'
     else
       # for macOS version is or newer than Big Sur
       # elsif MacOS.version <= :big_sur # reserved for later
-      sha256 '5ba1e2e8c230f1d98c9f855bc055593b62d54f4b84d49814f313ce28fddfe08d'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.2-mac-8.3/emacs-27.2-mac-8.3-11.6.1-spacemacs-icon.zip'
+      sha256 '2f412038d391ed663d3731c792ca586ae439d3442ed71f5a2719c3d635c1cbc2'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.1-mac-9.0/emacs-28.1-mac-9.0-11.6.5-spacemacs-icon.zip'
     end
   else
-    # Arm Macs should be running macOS >= Big Sur(11), let's add the version check in the future when necessary.
-    sha256 'bcacdeb6abdba2789f2b3d921185578b56203e95619cbb4b1a396c0ccadcd727'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-27.2-mac-8.3.1/emacs-27.2-mac-8.3.1-arm64-11.6.1-spacemacs-icon.zip'
+    if MacOS.version <= :big_sur
+      sha256 '00811da1b69f7edef017c3c28db8decf3e19ed4039829aa6239aeaefbb0ee106'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.1-mac-9.0/emacs-28.1-mac-9.0-arm64-11.6.5-spacemacs-icon.zip'
+    else
+      # for macOS version is or newer than Big Sur
+      # elsif MacOS.version <= :monterey # reserved for later
+      sha256 '89e28630ceb651824cb4df5a661e1c015fe511a0569ba1006f8af5868c9577a6'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.1-mac-9.0/emacs-28.1-mac-9.0-arm64-12.3.1-spacemacs-icon.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
