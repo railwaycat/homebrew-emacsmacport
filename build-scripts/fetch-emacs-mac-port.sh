ee79b048bbb2fd4a962dfb2204cc7a2f0d5237d8
Xin Xu <railwaycat@gmail.com>
2022-04-09 12:16:55 -0400

    fix on bundle structure for signing

--- build-scripts/fetch-emacs-mac-port.sh
+++ build-scripts/fetch-emacs-mac-port.sh
@@ -16,3 +16,9 @@ patch -d emacs-source -p1 < ../patches/emacs-mac-title-bar-9.0.patch
 
 echo "apply patch: multi-tty-27"
 patch -d emacs-source -p1 < ../patches/multi-tty-27.diff
+
+echo "apply patch: bundle structure signature hack"
+# this is a hack to let pdump be installed under Contents/MacOS and libexec
+# under Contents/MacOS/libexec. To avoid an error on bundle structure when
+# signing.
+patch -d emacs-source -p1 < ../patches/bundle-signature-hack-9.0.diff
