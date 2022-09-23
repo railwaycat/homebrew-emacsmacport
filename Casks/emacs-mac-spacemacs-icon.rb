ab49db149e044c3739ce9efd4468c1d923195676
Xin Xu <railwaycat@gmail.com>
2022-09-22 20:15:43 -0400

    fix the blocking of search on Catalina #309

--- Casks/emacs-mac-spacemacs-icon.rb
+++ Casks/emacs-mac-spacemacs-icon.rb
@@ -2,26 +2,22 @@ cask 'emacs-mac-spacemacs-icon' do
   version 'emacs-28.2-mac-9.1'
 
   if Hardware::CPU.intel?
-    if MacOS.version <= :catalina
-      odie "Because of the limitation of build system, there is no cask support for Catalina(10.15) and earlier, please build from Homebrew formula."
-    elsif MacOS.version <= :big_sur
+    depends_on macos: ">= :big_sur"
+
+    if MacOS.version <= :big_sur
       sha256 '8bd96c3500d8cbb02a9102baac76e34c88a1b9fd2ac6af295ceb0410788be712'
       url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-11.6.8-spacemacs-icon.zip'
     else
       # for macOS version is or newer than Big Sur
-      # elsif MacOS.version <= :big_sur # reserved for later
       sha256 '7693e13cab343f081210f78973cb8b13b9bf768734a989afb87c64adb1e5902b'
       url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-12.6-spacemacs-icon.zip'
     end
-  else
-    if MacOS.version <= :big_sur
-      odie "Because of the limitation of build system, there is no Apple Silicon cask support for Big Sur(11) and earlier, please build from Homebrew formula."
-    else
-      # for macOS version is or newer than Big Sur
-      # elsif MacOS.version <= :monterey # reserved for later
-      sha256 '90f8c924e3dabe9e5344190c8e841f4d918b447a47899011f1909728dd79b64b'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1-spacemacs-icon.zip'
-    end
+  else # Apple Silicon
+    depends_on macos: ">= :monterey"
+
+    # for macOS version is or newer than Monterey
+    sha256 '90f8c924e3dabe9e5344190c8e841f4d918b447a47899011f1909728dd79b64b'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1-spacemacs-icon.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -2,26 +2,22 @@ cask 'emacs-mac' do
   version 'emacs-28.2-mac-9.1'
 
   if Hardware::CPU.intel?
-    if MacOS.version <= :catalina
-      odie "Because of the limitation of build system, there is no cask support for Catalina(10.15) and earlier, please build from Homebrew formula."
-    elsif MacOS.version <= :big_sur
+    depends_on macos: ">= :big_sur"
+
+    if MacOS.version <= :big_sur
       sha256 '6a62f75894475fa3d9474cb5b7a9a596c5c5466b94398687b8a71d84fc3af6d8'
       url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-11.6.8.zip'
     else
       # for macOS version is or newer than Monterey
-      # elsif MacOS.version <= :monterey # reserved for later
       sha256 '484690f89fceaa03f686b38bff6c12f462bf26e1e8d61283fe22df30d1481482'
       url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-12.6.zip'
     end
-  else
-    if MacOS.version <= :big_sur
-      odie "Because of the limitation of build system, there is no Apple Silicon cask support for Big Sur(11) and earlier, please build from Homebrew formula."
-    else
-      # for macOS version is or newer than Monterey
-      # elsif MacOS.version <= :monterey # reserved for later
-      sha256 '488a1bf68a87a3dd304a25f2a1fe2f0b4ae95c2c80bfe4e53bdbeff0e3a2c5d4'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1.zip'
-    end
+  else # Apple Silicon
+    depends_on macos: ">= :monterey"
+
+    # for macOS version is or newer than Monterey
+    sha256 '488a1bf68a87a3dd304a25f2a1fe2f0b4ae95c2c80bfe4e53bdbeff0e3a2c5d4'
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1.zip'
   end
 
   appcast 'https://github.com/railwaycat/homebrew-emacsmacport/releases.atom'
