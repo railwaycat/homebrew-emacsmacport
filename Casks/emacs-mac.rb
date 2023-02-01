f94f597bebd04a0f7c9e629a07a66869eabf1488
Ruiyang Wu <ywwry66@gmail.com>
2023-01-31 23:30:56 -0500

    Put `man` files in the right place for the cask
    This complements #316 for the cask code. It also aligns with
    `emacs.rb` in the `homebrew-cask` repo. However, it does not deal with
    the `info` files since those would require external program
    `install-info` to work properly.

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -37,6 +37,10 @@ cask 'emacs-mac' do
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
   binary "#{appdir}/Emacs.app/Contents/Resources/include/emacs-module.h", target: "#{HOMEBREW_PREFIX}/include/emacs-module.h"
   binary "#{appdir}/Emacs.app/Contents/Resources/site-lisp/subdirs.el", target: "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/subdirs.el"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/etags.1.gz"
 
   zap trash: [
                '~/Library/Caches/org.gnu.Emacs',
