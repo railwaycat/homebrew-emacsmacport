95aaa6269dcdb6795852668e2ae062e89f3d1fc8
Xin Xu <railwaycat@gmail.com>
2023-08-08 16:18:00 -0400

    casks for emacs-29.1-mac-10.0

--- Casks/emacs-mac-spacemacs-icon.rb
+++ Casks/emacs-mac-spacemacs-icon.rb
@@ -1,23 +1,28 @@
 cask 'emacs-mac-spacemacs-icon' do
-  version 'emacs-28.2-mac-9.1'
+  version 'emacs-29.1-mac-10.0'
 
   if Hardware::CPU.intel?
     depends_on macos: ">= :big_sur"
 
     if MacOS.version <= :big_sur
-      sha256 '8bd96c3500d8cbb02a9102baac76e34c88a1b9fd2ac6af295ceb0410788be712'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-11.6.8-spacemacs-icon.zip'
+      sha256 '7f8f4923c38fefcb18ebb5922fb983bcca3b6328c75f6f8812787a7cdf77b9ee'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-x86_64-11.7.8-spacemacs-icon.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
     else
       # for macOS version is or newer than Big Sur
-      sha256 '7693e13cab343f081210f78973cb8b13b9bf768734a989afb87c64adb1e5902b'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-12.6-spacemacs-icon.zip'
+      sha256 '0d724e7bcd67ac8b16c50055df033220ffba570bd669ae209769d63e3dbe2a59'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-x86_64-12.6.7-spacemacs-icon.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
     end
   else # Apple Silicon
     depends_on macos: ">= :monterey"
 
-    # for macOS version is or newer than Monterey
-    sha256 '90f8c924e3dabe9e5344190c8e841f4d918b447a47899011f1909728dd79b64b'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1-spacemacs-icon.zip'
+    if MacOS.version <= :monterey
+      sha256 'dd3caddf159e855395abfa60561833af8bbb436354a27cd25df44b2b737da494'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-arm64-12.5.1-spacemacs-icon.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    else
+      # for macOS version is or newer than Monterey
+      sha256 'd47dddbf74a15d509a8217ed27a1a51bc9f19a35e7554d14dcdc7fbf4cb65f3e'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-arm64-13.4-spacemacs-icon.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    end
   end
 
   name 'Emacs-mac with spacemacs icon'
@@ -36,6 +41,10 @@ cask 'emacs-mac-spacemacs-icon' do
   binary "#{appdir}/Emacs.app/Contents/MacOS/bin/etags"
   binary "#{appdir}/Emacs.app/Contents/Resources/include/emacs-module.h", target: "#{HOMEBREW_PREFIX}/include/emacs-module.h"
   binary "#{appdir}/Emacs.app/Contents/Resources/site-lisp/subdirs.el", target: "#{HOMEBREW_PREFIX}/share/emacs/site-lisp/subdirs.el"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/ebrowse.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacs.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/emacsclient.1.gz"
+  manpage "#{appdir}/Emacs.app/Contents/Resources/man/man1/etags.1.gz"
 
   zap trash: [
                '~/Library/Caches/org.gnu.Emacs',
