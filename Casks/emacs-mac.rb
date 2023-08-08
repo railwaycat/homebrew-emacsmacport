b46f98523d2599a817f7e1b537e87e099b180e9f
Xin Xu <railwaycat@gmail.com>
2023-08-08 16:18:00 -0400

    casks for emacs-29.1-mac-10.0

--- Casks/emacs-mac.rb
+++ Casks/emacs-mac.rb
@@ -1,23 +1,28 @@
 cask 'emacs-mac' do
-  version 'emacs-28.2-mac-9.1'
+  version 'emacs-29.1-mac-10.0'
 
   if Hardware::CPU.intel?
     depends_on macos: ">= :big_sur"
 
     if MacOS.version <= :big_sur
-      sha256 '6a62f75894475fa3d9474cb5b7a9a596c5c5466b94398687b8a71d84fc3af6d8'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-11.6.8.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+      sha256 'f2f7f8230cb9db9db24fde1cde07928a99c05eb8d949662e761fe04cce029ca7'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-x86_64-11.7.8.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
     else
       # for macOS version is or newer than Monterey
-      sha256 '484690f89fceaa03f686b38bff6c12f462bf26e1e8d61283fe22df30d1481482'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-12.6.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+      sha256 'ac86abc88d66000bf92c845664d643f1c5f3595da9a328366952683e070d0d55'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-x86_64-12.6.7.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
     end
   else # Apple Silicon
     depends_on macos: ">= :monterey"
 
-    # for macOS version is or newer than Monterey
-    sha256 '488a1bf68a87a3dd304a25f2a1fe2f0b4ae95c2c80bfe4e53bdbeff0e3a2c5d4'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.2-mac-9.1/emacs-28.2-mac-9.1-arm64-12.5.1.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    if MacOS.version <= :monterey
+      sha256 '5224bb69e1f36d52284f22368507664096d7ab2cec360b370f08c29e0f3a9000'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-arm64-12.5.1.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    else
+      # for macOS version is or newer than ventura
+      sha256 '0caf0dd36115a2b648abc94ce715ca844071ced0ff6bac6dbed5c1d2fa66df24'
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-29.1-mac-10.0/emacs-29.1-mac-10.0-arm64-13.4.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    end
   end
 
   name 'Emacs-mac'
