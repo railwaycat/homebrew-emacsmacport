24fa8a130b01925f0c760d612dd7c4f8fb228b0a
Przemek Kryger <pkryger@gmail.com>
2023-11-21 09:42:48 +0000

    Use simple heuristics to select multi-tty patch in fetch-emacs-mac-port.sh

--- build-scripts/fetch-emacs-mac-port.sh
+++ build-scripts/fetch-emacs-mac-port.sh
@@ -6,9 +6,11 @@ MACPORT_VAR="$1"
 COMMIT_HASH="$2"
 OUTPUT_DIR="$3"
 
+MULTI_TTY_PATCH=emacs-mac-29-multi-tty.diff
 echo "fetch tarball"
 if [ "$1" = "head" ]; then
   git clone --depth=1 --branch=work https://bitbucket.org/mituharu/emacs-mac.git "$OUTPUT_DIR"
+  MULTI_TTY_PATCH=emacs-mac-29.2-rc-1-multi-tty.diff
 else
   curl -L "https://bitbucket.org/mituharu/emacs-mac/get/$COMMIT_HASH.tar.gz" -o "emacs-$MACPORT_VAR.tar.gz"
   mkdir "$OUTPUT_DIR"
@@ -19,7 +21,7 @@ echo "apply patch: --with-natural-title-bar"
 patch -d "$OUTPUT_DIR" -p1 < ../patches/emacs-mac-title-bar-9.1.patch
 
 echo "apply patch: multi-tty-27"
-patch -d "$OUTPUT_DIR" -p1 < ../patches/emacs-mac-29-multi-tty.diff
+patch -d "$OUTPUT_DIR" -p1 < ../patches/${MULTI_TTY_PATCH}
 
 echo "apply patch: bundle structure signature hack"
 # this is a hack to let pdump be installed under Contents/MacOS and libexec
