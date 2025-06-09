55bc918db847bfb89aa0fe30b00aa2a8ce52ec0b
sinxccc <railwaycat@gmail.com>
2025-06-09 11:47:40 -0400

    cleanup app build script for 29 and add a copy for 28

--- build-scripts/prepare_homebrew.sh
+++ build-scripts/prepare_homebrew.sh
@@ -1,32 +1,9 @@
 #!/bin/sh
 
-# Use a fresh homebrew under $HOME. May not needed in most of the
-# cases
-# new_homebrew () {
-#   BREW_DIR="$HOME/homebrew"
-
-#   mkdir "$BREW_DIR" && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C "$BREW_DIR"
-
-#   eval "$($BREW_DIR/bin/brew shellenv)"
-# }
-
 # disable all the automation to prevent unexpected time spending
 export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 
-# OS_VER=$(sw_vers -productVersion)
-
 set -e
 
 brew update --force --quiet
-
-# # brew update remove all installed formula and reinstall from
-# # Brewfile, prevent waste time on upgrade all
-# # brew remove --force $(brew list --formula)
-# brew bundle --force cleanup --file ./Brewfile-29
-brew bundle --file ./Brewfile-29
-# build gnutls from source code, to ensure the absolute path for
-# linked libs
-# brew install --build-from-source gnutls
-
-# brew list
