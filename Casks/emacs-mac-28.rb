7c99629657f22132c61cbe4c61329d18b05ca0ea
sinxccc <railwaycat@gmail.com>
2026-09-12 19:11:42 -0400

    casks: remove deprecated verified field. This change fix #425 (#426)
    * casks: remove deprecated verified field. This change fix #425
    
    * update workflow for the homebrew tap trust feature.

--- Casks/emacs-mac-28.rb
+++ Casks/emacs-mac-28.rb
@@ -4,18 +4,18 @@ cask 'emacs-mac-28' do
   if Hardware::CPU.intel?
     depends_on macos: :ventura # macOS 13
     sha256 '1ba349eb63edb0345c9514b76b1460023d0b59c118459f9f5e577be523d44d89'
-    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-x86_64-13.7.6.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+    url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-x86_64-13.7.6.zip'
   else # Apple Silicon
     depends_on macos: :sonoma # macOS 14
 
     if MacOS.version >= :sequoia # macOS 15
       # for macOS is or newer than 15
       sha256 '72e02759cc33334b5c897d2731f26f3cf81af9fc0985051c0e9274f3c9e471c8'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-arm64-15.5.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-arm64-15.5.zip'
     else
       # for macOS older than 15, which is 14
       sha256 '033c4d13ff16b408f8fa49cd021804eda1aa3db6e13d3f35ef67b9f63f425269'
-      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-arm64-14.7.6.zip', verified: "github.com/railwaycat/homebrew-emacsmacport"
+      url 'https://github.com/railwaycat/homebrew-emacsmacport/releases/download/emacs-28.3-rc1-mac-9.2/emacs-28.3-rc1-mac-9.2-arm64-14.7.6.zip'
     end
   end
 
